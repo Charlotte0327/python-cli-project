404251f3237e6c07b0983c798b808a5266ed39fe
Charlotte0327 <charlottegalloway40@gmail.com>
2026-08-27 15:44:15 -0500

    Adjust

--- src/sysrecon/main.py
+++ src/sysrecon/main.py
@@ -1,7 +1,25 @@
 import typer
+from rich import print
+from typing import Annotated
 
-def main(name: str):
-    print(f"Hello {name}!")
+app = typer.Typer()
+
+@app.command()
+
+def greeting(
+    name: Annotated[str, typer.Argument(help = "This is Optional")] = None, # Optional
+    lastname: str = typer.Argument(help = "This is Required --> "), # Required (Normally required before optional)
+    informal: bool = False
+    ):    # How these work by default 
+
+
+    if informal:   # Use --informal and prints infrmail greeting 
+        print(f"Hello {name} {lastname}!")
+    else:
+        print(f"Good day Ms. {name} {lastname}.")
+
+
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
+     
